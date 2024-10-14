0fd28158eb1fab36c1d52b6506f7f9c825a61a6f
sergio-251 <50610151+sergio-251@users.noreply.github.com>
2024-10-14 12:27:08 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,8 @@
+# Условная конструкция. Операторы if, elif, else
+first, second, third = int(input()), int(input()), int(input())
+if first == second and first == third and second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
